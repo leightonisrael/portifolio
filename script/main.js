5e7367fd928e9f7fc5a4303b5483e0a4f0121ed6
Israel Hall Leighton <leightonisrael2014@gmail.com>
2023-06-08 13:56:44 -0300

    adição da seção de projetos com responsividade

---
+++ script/main.js
@@ -0,0 +1,7 @@
+function clickMenu() {
+    if (menuBurguer.style.display == 'block') {
+        menuBurguer.style.display = 'none'
+    } else {
+        menuBurguer.style.display = 'block'
+    }
+}
