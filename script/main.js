6096132864d20de5069a78db2024346395fae57a
Israel Hall Leighton <leightonisrael2014@gmail.com>
2023-06-10 16:20:10 -0300

    pequenas alterações

--- script/main.js
+++ script/main.js
@@ -1,3 +1,12 @@
+/* function mudouTamanho() {
+    if (window.innerWidth <= 750) {
+        menuBurguer.style.display = 'none'
+    } else {
+        menuBurguer.style.display = 'block'
+    }
+}
+ */
+
 function clickMenu() {
     if (menuBurguer.style.display == 'block') {
         menuBurguer.style.display = 'none'
